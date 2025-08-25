bf642614fb02eb94b87550a927db36d8facc55c3
Mauricio Rincón <causemao@gmail.com>
2025-08-25 10:51:12 -0500

    Agregando logs para las trazas.

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/bootcamp/api/UserHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/bootcamp/api/UserHandler.java
@@ -7,6 +7,7 @@ import co.com.pragma.bootcamp.usecase.user.UserUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,7 @@ import java.util.Map;
 @Tag(name = "Usuarios", description = "Operaciones relacionadas con usuarios")
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class UserHandler {
 
     private final UserUseCase userUseCase;
@@ -27,13 +29,20 @@ public class UserHandler {
 
     @Operation(summary = "Registrar un nuevo usuario", description = "Crea un usuario en el sistema con datos personales básicos.")
     public Mono<ServerResponse> registrarUsuario(ServerRequest request) {
+        log.info("Iniciando registro de usuario");
         return request.bodyToMono(UserRequest.class)
+                .doOnNext(req -> log.debug("Payload recibido: {}", req))
                 .map(userDtoMapper::toDomain)
                 .flatMap(userUseCase::registrarUsuario)
                 .map(userDtoMapper::toResponse)
+                .doOnNext(res -> log.info("Usuario registrado exitosamente con ID: {}", res.getId()))
+                .doOnNext(
+                        res -> log.info("Usuario registrado exitosamente con documento: {}",
+                                res.getDocumentoIdentidad()))
                 .flatMap(response -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .bodyValue(response))
+                .doOnError(e -> log.error("Error registrando usuario", e))
                 .onErrorResume(BusinessException.class, e ->
                         ServerResponse.status(HttpStatus.CONFLICT)
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -43,19 +52,33 @@ public class UserHandler {
 
     @Operation(summary = "Listar todos los usuarios")
     public Mono<ServerResponse> listarUsuarios(ServerRequest request) {
+        log.info("Solicitud para listar todos los usuarios");
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(userUseCase.listarUsuarios().map(userDtoMapper::toResponse), UserResponse.class);
+                .body(
+                        userUseCase.listarUsuarios()
+                                .map(userDtoMapper::toResponse)
+                                .doOnNext(user -> log.debug("Usuario encontrado: {}", user))
+                                .doOnComplete(() -> log.info("Finalizada consulta de usuarios")),
+                        UserResponse.class
+                );
     }
 
     @Operation(summary = "Obtener usuario por documento de identidad")
     public Mono<ServerResponse> obtenerUsuarioPorDocumento(ServerRequest request) {
         String documento = request.pathVariable("documento");
+        log.info("Solicitud para obtener usuario con documento: {}", documento);
+
         return userUseCase.obtenerUsuarioPorDocumento(documento)
                 .map(userDtoMapper::toResponse)
+                .doOnNext(user -> log.info("Usuario encontrado: {}", user))
                 .flatMap(user -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .bodyValue(user))
-                .switchIfEmpty(ServerResponse.notFound().build());
+                .switchIfEmpty(
+                        ServerResponse.notFound().build()
+                                .doOnSuccess(r -> log.warn("Usuario con documento {} no encontrado", documento))
+                )
+                .doOnError(e -> log.error("Error al consultar usuario con documento {}", documento, e));
     }
 }
